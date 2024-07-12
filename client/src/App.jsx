e65ae7ee4409b9816789aca851144a73ffcd1a53
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-12 06:37:43 +0530

    create and add footer component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Projects from './pages/Projects'
 
 import React from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   
@@ -24,6 +25,7 @@ export default function App() {
         <Route path="/projects" element={<Projects />} />
 
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
