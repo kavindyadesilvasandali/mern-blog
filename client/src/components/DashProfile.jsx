f20dd741ad0eded1f4ea3a37c7ec9b7b5f42b7c9
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-13 17:13:56 +0530

    user image upload error alert

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,11 +1,22 @@
-import { Button, TextInput } from 'flowbite-react';
-import React, { useState } from 'react'
+import { Alert, Button, TextInput } from 'flowbite-react';
+import React, { useEffect, useRef, useState } from 'react'
 import {useSelector} from 'react-redux';
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+  } from 'firebase/storage';
+  import { app } from '../firebase';
 
 export default function DashProfile() {
     const {currentUser} = useSelector(state=> state.user)
     const [imageFile, setImageFile] = useState(null);
     const [imageFileUrl, setImageFileUrl]= useState(null);
+    const [imageFileUploadProgress, setImageFileUploadProgress]=useState(0);
+    const [imageFileUploadError, setImageFileUploadError]= useState(0);
+    console.log(imageFileUploadProgress, imageFileUploadError);
+    const filePickerRef = useRef();
     const handleImageChange =(e)=>{
         const file= e.target.files[0];
         if(file){
@@ -14,16 +25,64 @@ export default function DashProfile() {
         }
         
     };
-    console.log(imageFile, imageFileUrl);
+    useEffect(()=>{
+        if(imageFile){
+            uploadImage();
+        }
+    },[imageFile]);
+
+    const uploadImage= async() =>{
+        
+     //service firebase.storage {
+     //  match /b/{bucket}/o {
+     //  match /{allPaths=**} {
+     //  allow read;
+     //allow write:if
+     //request.resource.size < 2 * 1024 * 1024 &&
+     //request.resource.contentType.matched('image/.*')
+     //}
+     //}
+     //}
+
+       const storage = getStorage(app);
+       const fileName = new Date().getTime()+ imageFile.name;
+       const storageRef = ref(storage, fileName);
+       const uploadTask = uploadBytesResumable(storageRef, imageFile);
+       uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+            const progress=
+            (
+                snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+                setImageFileUploadProgress(progress.toFixed(0));
+            
+        },
+        (error)=>{
+            setImageFileUploadError('Could not upload image (File must be less than 2MB)');
+
+        },
+        ()=>{
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                setImageFileUrl(downloadURL);
+            });
+    }
+);
+       
+    
+    };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
      <h1 className='my-7 text-center font-semibold text-3xl'>Profile </h1>
      <form className='flex flex-col gap-4'>
-        <input type='file' accept='image/*' onChange={handleImageChange}/>
-        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full'>
+        <input type='file' accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={()=>
+            filePickerRef.current.click()
+        }>
         <img src={imageFileUrl || currentUser.profilePicture}alt='user'
         className='rounded-full w-full h-full object-cover border-8 border-[lightgray]'/>
         </div>
+        {imageFileUploadError && <Alert color='failure'>{imageFileUploadError}</Alert>}
         <TextInput type='text' id='username' placeholder='username'
         defaultValue={currentUser.username}/>
         <TextInput type='email' id='email' placeholder='email'
