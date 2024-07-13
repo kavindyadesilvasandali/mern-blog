b1985d2a40aed69457b39336ce1ba64f809e7e9b
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-13 12:21:26 +0530

    complete dark mode functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -3,10 +3,13 @@ import userReducer from './user/userSlice';
 import {persistReducer} from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { version } from 'mongoose';
+import themeReducer from './theme/themeSlice';
 import persistStore from 'redux-persist/es/persistStore';
+import { theme } from 'flowbite-react';
 
 const rootReducer = combineReducers({
     user:userReducer,
+    theme:themeReducer,
 });
 
 const persistConfig ={
