1bfb3aa5db142a27609d3a2505d4249b75516d2f
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-22 09:36:37 +0530

    add admin functionality to the user

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { type } from "os";
 
 const userSchema = new mongoose.Schema({
     username:{
@@ -18,6 +19,10 @@ const userSchema = new mongoose.Schema({
     profilePicture:{
       type:String,
       default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fid.pinterest.com%2Fstrawberryblushing%2Faesthetic-blank-profile-picture%2F&psig=AOvVaw1x4VvHvhlWSdA_CXCOJuzn&ust=1720890194557000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCOiGz9z9oYcDFQAAAAAdAAAAABAE",  
+    },
+    isAdmin:{
+        type: Boolean,
+        default:false,
     },
 },{timestamps:true}
 );
