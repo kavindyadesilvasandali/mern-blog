196eb0e4c91b7774f2c899c34e834b4e047b249d
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-24 23:43:34 +0530

    add show more functionality to the posts results of the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -5,7 +5,7 @@ import { Table } from 'flowbite-react';
 export default function DashPosts() {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  //const [showMore, setShowMore] = useState(true);
+  const [showMore, setShowMore] = useState(true);
   //const [showModal, setShowModal] = useState(false);
   //const [postIdToDelete, setPostIdToDelete] = useState('');
   useEffect(() => {
@@ -27,6 +27,24 @@ export default function DashPosts() {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
   
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
