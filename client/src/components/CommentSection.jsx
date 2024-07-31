87cf3c33ff1022c30c5c0a37de0d69635755893d
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-31 21:59:16 +0530

    add like functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -12,6 +12,8 @@ export default function CommentSection(postId) {
     const [comment, setComment] = useState('');
     const [commentError, setCommentError] = useState(null);
     const [comments, setComments] = useState([]);
+    const navigate = useNavigate();
+    console.log(comment);
     const handleSubmit = async (e) => {
         e.preventDefault();
         if (comment.length > 200) {
@@ -54,6 +56,34 @@ export default function CommentSection(postId) {
         };
         getComments();
       }, [postId]);
+
+      const handleLike = async (commentId) => {
+        try {
+          if (!currentUser) {
+            navigate('/sign-in');
+            return;
+          }
+          const res = await fetch(`/api/comment/likeComment/${commentId}`, {
+            method: 'PUT',
+          });
+          if (res.ok) {
+            const data = await res.json();
+            setComments(
+              comments.map((comment) =>
+                comment._id === commentId
+                  ? {
+                      ...comment,
+                      likes: data.likes,
+                      numberOfLikes: data.likes.length,
+                    }
+                  : comment
+              )
+            );
+          }
+        } catch (error) {
+          console.log(error.message);
+        }
+      };
 
   return (
     
