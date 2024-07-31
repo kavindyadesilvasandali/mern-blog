dc1cc954ff6daa9438f1cb9acd93ae30ec86190e
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-31 20:58:22 +0530

    like functionality to the comment backend

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -22,11 +22,12 @@ export default router;*/
 
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
-import {    createComment , getPostComments } from '../controllers/comment.controller.js';
+import {    createComment , getPostComments, likeComment } from '../controllers/comment.controller.js';
 
 const router= express.Router();
 
 router.post('/create',verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
 
 export default router;
