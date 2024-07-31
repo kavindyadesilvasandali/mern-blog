a5002ff018c889312bfea4e16e9bfb10b967c7d6
kavindyadesilvasandali <kavindyadesilvas2001@gmail.com>
2024-07-31 20:45:57 +0530

    show the comments of a post

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { test, updateUser , deleteUser, signout, getUsers} from '../controllers/user.controller.js';
+import { test, updateUser , deleteUser, signout, getUsers, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
@@ -10,5 +10,6 @@ router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
 router.get('/getusers', verifyToken, getUsers);
 router.delete('/deleteusers', verifyToken, deleteUser);
+router.get('/:userId', getUser);
 
 export default router;
